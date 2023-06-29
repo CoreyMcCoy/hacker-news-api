92475b8f2fa54ab094ae81831a8af8f012095b73
CoreyMcCoy <coreyamccoy@gmail.com>
2023-06-28 21:45:19 -0500

    Updated function

--- app.js
+++ app.js
@@ -8,39 +8,49 @@ const ul2 = document.querySelector('.second-ul');
 const getData = async () => {
   // Set the search parameter from the input value
   const searchTerm = input.value;
-  const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${searchTerm}.json?print=pretty`);
+  const response = await fetch(
+    `https://hacker-news.firebaseio.com/v0/user/${searchTerm}.json?print=pretty`
+  );
   let data = await response.json();
-  const { by, id, kids, text, title, type } = data;
+  const { about, id, karma, submitted } = data;
 
-  // Create a new list item and add it to the first list
-  const li = document.createElement('li');
-  const result = document.querySelector('#result');
-  if (title !== undefined) {
-    result.innerHTML = `<a href=' https://hacker-news.firebaseio.com/v0/user/${by}.json?print=pretty'>${by}</a> wrote -- ${title}`;
-  } else {
-    result.innerHTML = `<a href=' https://hacker-news.firebaseio.com/v0/user/${by}.json?print=pretty'>${by}</a> wrote`;
-  }
-  if (text !== undefined) {
-    li.classList.add('originalText');
-    li.innerHTML += `<p>${text}</p>`;
-  }
-  ul1.appendChild(li);
+  console.log(data);
 
-  // Display comments and append them to the second list
-  if (kids === undefined) {
-    const comments = document.querySelector('#comments');
-    comments.innerHTML = 'No comments on this post yet';
-  } else {
-    const kiddos = kids.forEach(async (kid) => {
-      const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${kid}.json?print=pretty`);
-      const data = await response.json();
-      const { text } = data;
-      const lis = document.createElement('li');
-      lis.classList.add('comment-group');
-      lis.innerHTML += `<p><span>🔺</span> ${text}</p>`;
-      ul2.appendChild(lis);
-    });
-  }
+  // Display the data in the DOM
+  ul1.innerHTML = `
+    <li><strong>Username:</strong> ${id}</li>
+    <li><strong>Karma:</strong> ${karma}</li>
+    <li><strong>About:</strong> ${about}</li>
+  `;
+
+  // Loop over the submit array and display them in the DOM
+  submitted.forEach((item) => {
+    // Make the API call for each item in the array and get the title and make it a link
+    const getTitles = async () => {
+      const response = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`
+      );
+      let data = await response.json();
+      console.log(data);
+      let { title, text, id, kids } = data;
+
+      if (text === undefined) {
+        text = 'No text available';
+      }
+
+      if (kids === undefined) {
+        kids = 'No kids available';
+      }
+
+      ul2.innerHTML += `
+        <li><strong>ID:</strong> ${id}</li>
+        <li>Title: <a href="https://news.ycombinator.com/item?id=${item}">${title}</a></li>
+        <li><strong>Text:</strong> ${text}</li>
+      `;
+    };
+    getTitles();
+  });
+
   // Clear the input value and reset the form
   input.value = '';
   input.addEventListener('click', (e) => {
